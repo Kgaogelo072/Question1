8a90ef91265c957fe27e4c6740ebfe486a9fe5ef
Kgaogelo072 <140261103+Kgaogelo072@users.noreply.github.com>
2024-04-05 13:48:36 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+
+public class LongestSentanceAnswer
+
+{       static void Main()
+    {
+        Console.WriteLine(Solution());
+
+    }
+        public static int Solution()
+        {
+            //string s = "We test coders. Give us a try?"
+            string s = "Forget CSs..Save time .xx";
+            string[] sentences = s.Split(new char[] { '.', '?', '!' }, StringSplitOptions.RemoveEmptyEntries);
+            int maxLength = 0;
+            foreach (string sentence in sentences)
+            {
+                string[] words = sentence.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                int sentenceLength = words.Length;
+                if (sentenceLength > maxLength)
+                {
+                    maxLength = sentenceLength;
+                }
+            }
+            return maxLength;
+        }
+}
+
